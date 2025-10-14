8949b9e93ce1bd8da909e984b6a52b04126b7d31
José Neilson <joseneilson13@gmail.com>
2025-10-14 14:11:29 -0300

    add chatroom channel through ActionTable

---
+++ app/javascript/channels/chatroom_channel.js
@@ -0,0 +1,15 @@
+import consumer from "channels/consumer"
+
+consumer.subscriptions.create({ channel: "ChatroomChannel" }, {
+  connected() {
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received(data) {
+
+  }
+});
